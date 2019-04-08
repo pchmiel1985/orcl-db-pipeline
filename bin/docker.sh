f2d726df547fdc0245d23bde5ae8f75e399fe723
Paweł Chmiel <pawel.chmiel@oracle.com>
2019-04-08 14:48:05 +0200

    dockerfiles dla oracle database 12.2.0.1

--- bin/docker.sh
+++ bin/docker.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-pwd ${pwd}
-cd $pwd/docker/OracleDatabase/dockerfiles/
+pwd=${PWD}
+echo $pwd
+cd ../docker/OracleDatabase/dockerfiles/
 
 ./buildImage.sh $1
 
-cd $pwd/docker/OracleDatabase/
+cd $pwd
+cd ../docker/OracleDatabase/
 
 echo "create container"
