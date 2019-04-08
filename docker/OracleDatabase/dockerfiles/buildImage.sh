5d50fc0e72299b75ce0405099238701a42c5c4d2
Paweł Chmiel <pawel.chmiel@oracle.com>
2019-04-08 14:03:40 +0200

    dockerfiles dla oracle database 12.2.0.1

--- docker/OracleDatabase/dockerfiles/buildImage.sh
+++ docker/OracleDatabase/dockerfiles/buildImage.sh
@@ -4,7 +4,7 @@ cd 12.2.0.1
 
 echo "................................................."
 echo "Rozpoczęto kopiowanie linuxx64_12201_database.zip"
-cp "$1/linuxx64_12201_database.zip" .
+cp -n "$1/linuxx64_12201_database.zip" .
 echo "................................................."
 
 docker build -t "orcl:12.2.0.1-ee" -f Dockerfile.ee . || {
