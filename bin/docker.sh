5e51ff790a6ba882f1e4cc272beeb615f4aa581f
Paweł Chmiel <pawel.chmiel@oracle.com>
2019-04-08 14:57:06 +0200

    dockerfiles dla oracle database 12.2.0.1

--- bin/docker.sh
+++ bin/docker.sh
@@ -5,9 +5,9 @@ imageName="orcl:12.2.0.1-ee"
 
 #Utwórz obraz bazydanych dla docker
 cd ../docker/OracleDatabase/dockerfiles/
-./buildImage.sh $1 $imageName
+. ./buildImage.sh $1 $imageName
 
 #Utwórz kontener bazydanych w docker
 cd $pwd
 cd ../docker/OracleDatabase/
-./createContainer.sh $imageName
+. ./createContainer.sh $imageName
