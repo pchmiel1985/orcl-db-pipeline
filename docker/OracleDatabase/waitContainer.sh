a734b28a265ac7b9cef8c8a833efa1dbf3617467
Paweł Chmiel <pawel.chmiel@oracle.com>
2019-04-08 17:28:25 +0200

    dockerfiles dla oracle database 12.2.0.1

--- docker/OracleDatabase/waitContainer.sh
+++ docker/OracleDatabase/waitContainer.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-while [[ $i -lt 1 ]]
+while [[ $i -lt 2 ]]
 do
 i=$(docker logs --tail 20 $1 |grep -c "Completed:    ALTER PLUGGABLE DATABASE PMUL SAVE STATE")
-i=$[$i+$(docker logs --tail 20 $1 |grep -c "Pluggable database PMUL opened read write")]
 i=$[$i+$(docker logs --tail 20 $1 |grep -c "DATABASE IS READY TO USE!")]
-sleep 1
+i=$[$i+$(docker ps |grep mul |grep -c "(healthy)")]
+
 j=$[$j+1]
 if [[ $j -gt $2 ]]; then
     echo "Przekroczono limit oczekiwania"
     exit 1
 else
+    sleep 1
     echo "oczekiwanie - $j"
 fi
 done
