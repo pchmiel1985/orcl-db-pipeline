0dccf60597d572294ce36deee824031526dd8c9a
Paweł Chmiel <pawel.chmiel@oracle.com>
2019-04-08 13:00:20 +0200

    dockerfiles dla oracle database 12.2.0.1

--- docker/OracleDatabase/dockerfiles/buildImage.sh
+++ docker/OracleDatabase/dockerfiles/buildImage.sh
@@ -2,6 +2,10 @@
 
 cd 12.2.0.1
 
+#kopiuj plik z lokalizacji
+#todo do zmiany hardcode na parametr
+cp /home/file_store/linuxx64_12201_database.zip .
+
 docker build -t "orcl:12.2.0.1-ee" -f Dockerfile.ee . || {
   echo "Nastapił błąd podczas budowania obrazu."
   exit 1
